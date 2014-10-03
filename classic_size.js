ca22b9c90e5e564c20d9f364781ba0746b0d4d38
byronhulcher <byronhulcher@gmail.com>
2014-10-02 20:01:50 -0400

    Version 0.0.3 Won't create unnessecary elements when the bookmarklet is clicked multiple times

--- classic_size.js
+++ classic_size.js
@@ -1,16 +1,21 @@
-var style = document.createElement('style');
-style.appendChild(document.createTextNode('body.classic-size video{width:135px;height:100px}body.classic-size #video-preview{width:135px;height:100px}body.classic-size #padding{height:102px}body.classic-size li{height:100px}body.classic-size li p{height:83px}body.classic-size .debugger{bottom:25px;left:55px}body.classic-size #composer-message{margin-left:140px}')); 
-document.head.appendChild(style);
 document.body.className += ' classic-size';
-var button = document.createElement('button');
-button.id = 'change-size';
-button.appendChild(document.createTextNode('Change Size'));
-button.onclick = function(){
-  if (document.body.className.indexOf('classic-size') > -1){
-    document.body.className = document.body.className.replace('classic-size', '');
-  }
-  else {
-    document.body.className += ' classic-size';
-  }
-};
-document.getElementById('menu').appendChild(button);
+if (!document.getElementById('meat-classic-css')){
+  var style = document.createElement('style');
+  style.id = "meat-classic-css";
+  style.appendChild(document.createTextNode('body.classic-size video{width:135px;height:100px}body.classic-size #video-preview{width:135px;height:100px}body.classic-size #padding{height:102px}body.classic-size li{height:100px}body.classic-size li p{height:83px}body.classic-size .debugger{bottom:25px;left:55px}body.classic-size #composer-message{margin-left:140px}')); 
+  document.head.appendChild(style);
+}
+if (!document.getElementById('change-size')){
+  var button = document.createElement('button');
+  button.id = 'change-size';
+  button.appendChild(document.createTextNode('Change Size'));
+  button.onclick = function(){
+    if (document.body.className.indexOf('classic-size') > -1){
+      document.body.className = document.body.className.replace(/classic-size/g, '');
+    }
+    else {
+      document.body.className += ' classic-size';
+    }
+  };
+  document.getElementById('menu').appendChild(button);
+}
