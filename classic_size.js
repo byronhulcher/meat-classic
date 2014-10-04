896bd6d42a7f20b933f35375707772560ec84651
byronhulcher <byronhulcher@gmail.com>
2014-10-04 14:04:05 -0400

    Version 0.0.4 shaved 100 bytes off bookmarklet, fixed timestamp css

--- classic_size.js
+++ classic_size.js
@@ -1,21 +1,16 @@
-document.body.className += ' classic-size';
-if (!document.getElementById('meat-classic-css')){
-  var style = document.createElement('style');
-  style.id = "meat-classic-css";
-  style.appendChild(document.createTextNode('body.classic-size video{width:135px;height:100px}body.classic-size #video-preview{width:135px;height:100px}body.classic-size #padding{height:102px}body.classic-size li{height:100px}body.classic-size li p{height:83px}body.classic-size .debugger{bottom:25px;left:55px}body.classic-size #composer-message{margin-left:140px}')); 
-  document.head.appendChild(style);
+document.body.className += ' classic';
+if (!document.getElementById('classic-css')){
+  var s = document.createElement('style');
+  s.id = "classic-css";
+  s.appendChild(document.createTextNode('.classic video{width:135px;height:100px}.classic #video-preview{width:135px;height:100px}.classic #padding{height:102px}.classic li{height:100px}.classic li p{height:83px}.classic .debugger{bottom:25px;left:55px}.classic #composer-message{margin-left:140px}.classic .timestamp{left:auto}')); 
+  document.head.appendChild(s);
 }
-if (!document.getElementById('change-size')){
-  var button = document.createElement('button');
-  button.id = 'change-size';
-  button.appendChild(document.createTextNode('Change Size'));
-  button.onclick = function(){
-    if (document.body.className.indexOf('classic-size') > -1){
-      document.body.className = document.body.className.replace(/classic-size/g, '');
-    }
-    else {
-      document.body.className += ' classic-size';
-    }
+if (!document.getElementById('toggle-classic')){
+  var b = document.createElement('button');
+  b.id = 'toggle-classic';
+  b.appendChild(document.createTextNode('Change Size'));
+  b.onclick = function(){
+    document.body.className = document.body.className.indexOf('classic') > -1 ? document.body.className.replace(/classic/g, '') : document.body.className += ' classic';
   };
-  document.getElementById('menu').appendChild(button);
+  document.getElementById('menu').appendChild(b);
 }
